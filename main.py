4c70c4ddfdabc89db1a5846a57760c4b64aa3529
Adel Qalieh <aqalieh95@gmail.com>
2020-06-02 02:08:22 -0400

    Save each note type into a folder to set up for templates

--- main.py
+++ main.py
@@ -6,21 +6,24 @@ ANKI_BASE_PATH = os.path.expanduser("~/.local/share/Anki2/")
 COLLECTION_PATH = ANKI_BASE_PATH + "{}/collection.anki2"
 
 
-def save_collection_css(collection):
+def save_collection_css(collection, output="output"):
     db_path = COLLECTION_PATH.format(collection)
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute("SELECT models from col")
 
-    os.makedirs(collection, exist_ok=True)
     models = json.loads(c.fetchone()[0])
     for model in models.values():
+        # Create folder for model
         name = model["name"]
-        css = model["css"]
         # Forward slashes mess up file paths
-        filename = "{}.css".format(name.replace("/", "|"))
-        filepath = os.path.join(collection, filename)
-        with open(filepath, "w") as f:
+        clean_name = name.replace("/", "|")
+        # Ex: output/M2/Cloze-AQ/
+        dirpath = os.path.join(output, collection, clean_name)
+        os.makedirs(dirpath, exist_ok=True)
+        # Save css
+        css = model["css"]
+        with open(os.path.join(dirpath, "index.css"), "w") as f:
             f.write(css)
 
 
